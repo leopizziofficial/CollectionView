0bb1ca98e5e4647e29f95f1a7703ed11a122d35f
Giacomo Leopizzi <giacomo@leopizzi.com>
2021-12-06 23:35:28 +0100

    SectionBuilder public

--- Sources/CollectionView/SectionBuilder.swift
+++ Sources/CollectionView/SectionBuilder.swift
@@ -10,15 +10,15 @@ import Foundation
 @resultBuilder
 public struct CVSectionBuilder {
     
-    static func buildArray(_ components: [[CVSection]]) -> [CVSection] {
+    static public func buildArray(_ components: [[CVSection]]) -> [CVSection] {
         return components.reduce([], +)
     }
 
-    static func buildBlock(_ components: [CVSection]...) -> [CVSection] {
+    static public func buildBlock(_ components: [CVSection]...) -> [CVSection] {
         return components.reduce([], +)
     }
     
-    static func buildExpression(_ expression: CVSection) -> [CVSection] {
+    static public func buildExpression(_ expression: CVSection) -> [CVSection] {
         return [expression]
     }
     
